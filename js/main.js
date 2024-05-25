7b0f23ab1cdc7a716438b76da4fcae7a7ad27887
JuanJAreiza <105412915+JuanJAreiza@users.noreply.github.com>
2024-05-25 18:55:42 -0500

    Se mejora Horarios Semanal y Mensual, al igual que se termina de pulir todo el apartado de Notas

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector('.menu_btn');
 const menuBtnIcon = document.querySelector('.menu_btn i');
 const dropDownMenu = document.querySelector('.dropdown_menu');
 
+//HEADER: FUNCION PARA EL DESPLIEGUE DEL BURGUER BUTTON
 menuBtn.onclick = function () {
      dropDownMenu.classList.toggle('open')
      const isOpen = dropDownMenu.classList.contains('open')
@@ -10,3 +11,23 @@ menuBtn.onclick = function () {
      ? 'fa-solid fa-xmark'
      : 'fa-solid fa-bars'
 }
+
+
+//NOTAS: FUNCION PARA EL DESPLIEGUE DE TABLAS
+document.addEventListener('DOMContentLoaded', () => {
+     const mainRows = document.querySelectorAll('.main-row');
+ 
+     mainRows.forEach(mainRow => {
+         mainRow.addEventListener('click', () => {
+
+          //LINEA PARA MANTENER ACTIVO EL COLOR EN EL ITEM SELECCIONADO
+          mainRow.classList.toggle('active');
+
+          //LINEA PARA DESPLEGAR Y CONTRAER TABLAS
+             const nextElement = mainRow.nextElementSibling;
+             if (nextElement && nextElement.classList.contains('details-row')) {
+                 nextElement.style.display = nextElement.style.display === 'table-row' ? 'none' : 'table-row';
+             }
+         });
+     });
+ });
